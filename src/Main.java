8ff8cbd1f1b23d96cfc8bbdf4076dc6a7bc9ab33
Jaune-Senat <tenarindia@gmail.com>
2025-12-08 16:31:46 +0100

    Exo 4 avec un vrai random
    Signed-off-by: Jaune-Senat <tenarindia@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int rand = 67;
+        Random rand = new Random();
+        int math = rand.nextInt(1,100);
         int guess = 0;
 
-        while (guess != rand){
+        while (guess != math){
             System.out.println("Indiquez un entier compris entre 1 et 100");
             guess = scanner.nextInt();
 
-            if(guess > rand){
+            if(guess > math){
                 System.out.println(guess + " est trop grand");
-            } else if (guess < rand) {
+            } else if (guess < math) {
                 System.out.println(guess + " est trop petit");
             }else {
                 System.out.println(guess + " est la bonne réponse");
