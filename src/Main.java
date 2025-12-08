2e4f8ec52295985f4bff838237c665568d9d840a
Jaune-Senat <tenarindia@gmail.com>
2025-12-08 12:44:24 +0100

    Exo2

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Entrez un nombre");
+        System.out.println("Entrez la première note");
 
-        int nombre = scanner.nextInt();
+        int nb1 = scanner.nextInt();
 
-        if (nombre %2 == 1){
-            System.out.println("Ce nombre est impair");
+        System.out.println("Entrez la deuxième note");
+
+        int nb2 = scanner.nextInt();
+
+        System.out.println("Entrez la troisième note");
+
+        int nb3 = scanner.nextInt();
+
+        int moyenne = (nb1 +nb2 +nb3)/3 ;
+
+        if (moyenne >= 10){
+            System.out.println("Vous êtes admis");
         }else {
-            System.out.println("Ce nombre est pair");
+            System.out.println("Vous êtes ajourné");
         }
     }
 }
